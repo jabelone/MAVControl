11f2787990326b8f69fdafe16f20c1b023c14314
Jaimyn Mayer <hello@jaimyn.com.au>
2018-01-06 18:30:01 +1000

    all the actions tab buttons on click handlers implemented (no mavlink)

--- static/js/main.js
+++ static/js/main.js
@@ -109,14 +109,89 @@ $(document).ready(function () {
     $('.modal').modal();
     $('select').material_select();
 
+
+    // ====== Actions Tab Stuff ======
+    // Do Action
+    document.getElementById("do_action").addEventListener('click', function () {
+        let action = document.getElementById("actions_select").value;
+        // TODO implement sending mavlink packet for action
+        Materialize.toast('Did ' + action, 2000);
+    }, false);
+
+    // Do Action
+    document.getElementById("set_wp").addEventListener('click', function () {
+        let wp = document.getElementById("wp_select").value;
+        // TODO implement sending mavlink packet for wp
+        Materialize.toast('Set WP to ' + wp, 2000);
+    }, false);
+
+    // Set Mode
+    document.getElementById("set_mode").addEventListener('click', function () {
+        let mode = document.getElementById("mode_select").value;
+        // TODO implement sending mavlink packet for set mode
+        Materialize.toast(mode + " mode", 2000);
+    }, false);
+
+    // Auto Mode Button
+    document.getElementById("auto_button").addEventListener('click', function () {
+        // TODO implement sending mavlink packet for auto mode
+        Materialize.toast("Auto mode", 2000);
+    }, false);
+
+    // RTL Mode Button
+    document.getElementById("rtl_button").addEventListener('click', function () {
+        // TODO implement sending mavlink packet for rtl mode
+        Materialize.toast("RTL mode", 2000);
+    }, false);
+
+    // Loiter Mode Button
+    document.getElementById("loiter_button").addEventListener('click', function () {
+        // TODO implement sending mavlink packet for loiter mode
+        Materialize.toast("Loiter mode", 2000);
+    }, false);
+
+    // Arm Button
+    document.getElementById("arm_button").addEventListener('click', function () {
+        // TODO implement sending mavlink packet for arming
+        Materialize.toast("ARMED!!", 3000);
+    }, false);
+
+    // Disarm Button
+    document.getElementById("disarm_button").addEventListener('click', function () {
+        // TODO implement sending mavlink packet for disarming
+        Materialize.toast("DISARMED!!", 3000);
+    }, false);
+
     // ====== Servo Tab Stuff ======
     let servo_toggle = document.getElementsByClassName("servo_toggle");
     for (let i = 0; i < servo_toggle.length; i++) {
         servo_toggle[i].addEventListener('click', function () {
             let servo_toggled = $(this).data("servo_number");
+            // TODO implement sending mavlink packet for servo control
             Materialize.toast('Servo ' + servo_toggled + " toggled", 2000);
         }, false);
     }
+
+    // Set Airspeed
+    document.getElementById("change_speed_button").addEventListener('click', function () {
+        let speed = document.getElementById("change_speed_value").value;
+        // TODO implement sending mavlink packet for set altitude
+        Materialize.toast("Set target airspeed to " + speed, 3000);
+    }, false);
+
+    // Set Altitude
+    document.getElementById("change_altitude_button").addEventListener('click', function () {
+        let altitude = document.getElementById("change_altitude_value").value;
+        // TODO implement sending mavlink packet for set altitude
+        Materialize.toast("Set target altitude to " + altitude, 3000);
+    }, false);
+
+    // Set Loiter Radius
+    document.getElementById("loiter_radius_button").addEventListener('click', function () {
+        let radius = document.getElementById("loiter_radius_value").value;
+        // TODO implement sending mavlink packet for set loiter radius
+        Materialize.toast("Set loiter radius to " + radius, 3000);
+    }, false);
 
     for (let i = 5; i < 15; i++) {
         let number = i;
@@ -155,6 +230,7 @@ $(document).ready(function () {
     for (let i = 0; i < servo_high.length; i++) {
         servo_high[i].addEventListener('click', function () {
             let servo_toggled = $(this).data("servo_number");
+            // TODO implement sending mavlink packet for servo high
             Materialize.toast('Servo ' + servo_toggled + " high", 2000);
         }, false);
     }
@@ -164,6 +240,7 @@ $(document).ready(function () {
     for (let i = 0; i < servo_low.length; i++) {
         servo_low[i].addEventListener('click', function () {
             let servo_toggled = $(this).data("servo_number");
+            // TODO implement sending mavlink packet for servo low
             Materialize.toast('Servo ' + servo_toggled + " low", 2000);
         }, false);
     }
