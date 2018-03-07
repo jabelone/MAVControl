81233b1171d75e01afb15083d1b618d2780f10a9
jabelone <hello@jaimyn.com.au>
2018-03-07 12:41:56 +1000

    Simplified get_mode_string() method

--- utilities.py
+++ utilities.py
@@ -23,45 +23,10 @@ def get_vehicle_string():
 def get_mode_string():
     if cs.vehicle_type is not None and cs.mode_type_enum is not None:
         if cs.vehicle_type == "Plane":
-            if cs.mode_type_enum == 0:
-                return "Manual"
-            elif cs.mode_type_enum == 1:
-                return "Circle"
-            elif cs.mode_type_enum == 2:
-                return "Stabilise"
-            elif cs.mode_type_enum == 3:
-                return "Training"
-            elif cs.mode_type_enum == 4:
-                return "Acro"
-            elif cs.mode_type_enum == 5:
-                return "FBWA"
-            elif cs.mode_type_enum == 6:
-                return "FBWB"
-            elif cs.mode_type_enum == 7:
-                return "Cruise"
-            elif cs.mode_type_enum == 8:
-                return "Autotune"
-            elif cs.mode_type_enum == 10:
-                return "Auto"
-            elif cs.mode_type_enum == 11:
-                return "RTL"
-            elif cs.mode_type_enum == 12:
-                return "Loiter"
-            elif cs.mode_type_enum == 14:
-                return "AvoidADSB"
-            elif cs.mode_type_enum == 15:
-                return "Guided"
-            elif cs.mode_type_enum == 16:
-                return "Initializing"
-            elif cs.mode_type_enum == 17:
-                return "QStabilise"
-            elif cs.mode_type_enum == 18:
-                return "QHover"
-            elif cs.mode_type_enum == 19:
-                return "QLoiter"
-            elif cs.mode_type_enum == 20:
-                return "QLand"
-            elif cs.mode_type_enum == 21:
-                return "QRTL"
+            mode = mavutil.mode_mapping_apm[cs.mode_type_enum]
+            return mode
+        if cs.vehicle_type == "Copter":
+            mode = mavutil.mode_mapping_acm[cs.mode_type_enum]
+            return mode
 
     return "Unknown"
